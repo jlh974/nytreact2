fa17a86d39e881376002e1d3d2d74188ad1a7ee0
Jeannine Hall <jeanninehall@Jeannines-MacBook-Pro.local>
2017-06-24 08:09:07 -0500

    final commit

---
+++ app/components/children/grandchildren/Saved.js
@@ -0,0 +1,19 @@
+// Include React
+var React = require("react");
+
+var Saved = React.createClass({
+  render: function() {
+    return (
+      <div className="panel panel-warning">
+        <div className="panel-heading">
+          <h3 className="panel-title">Saved Articles</h3>
+        </div>
+        <div className="panel-body">
+          
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Saved;
